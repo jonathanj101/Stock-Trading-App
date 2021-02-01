225b7ac17955755ae41e9fc39caa119b3ef9d225
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-01 15:30:34 -0500

    re-implementing React-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
+
+  )
 }
 
 export default App;
