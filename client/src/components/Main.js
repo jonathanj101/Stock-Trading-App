49e68dde91cc7298a0a8920c107942f8e60a210d
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-01 20:29:37 -0500

    implemented Route and Switch lib, imported Summary Component

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { Route, Switch} from 'react-router-dom'
 import Home from '../pages/Home'
 import NavbarComponent from './NavbarComponent'
 import Footer from './Footer'
+import SummaryComponent from '../pages/Summary'
 
 
 class Main extends Component {
@@ -15,7 +17,10 @@ class Main extends Component {
         return (
             <div>
                 <NavbarComponent />
-                <Home />
+                <Switch>
+                    <Route path="/" exact component={() => <Home />} />
+                    <Route path="/summary" exact component={() => <SummaryComponent />} />
+                </Switch>
                 <Footer />
             </div>
         )
