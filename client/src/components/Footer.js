a0c6165a0826fd750f45a96d3acc0015a1bc3bd0
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-02 20:13:48 -0500

    finished with styling, component done

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,23 +4,19 @@ const Footer = () => {
   return (
       <div
           className="bg-dark w-100 h-100"
-          style={textSize}  >
+          style={{fontSize: "1.5rem"}}  >
       <div className="w-50 mx-auto text-center text-white">
         <div>
           <p className="fs-1">@Jonathan J || Full Stack Developer</p>
         </div>
-        <div className="">
-          <a href="https://www.linkedin.com/in/jonathan-jimenez101/">
-            <i className="fab fa-linkedin"></i>
+        <div >
+          <a className="lindkedIn_div"  href="https://www.linkedin.com/in/jonathan-jimenez101/">
+            <i  className="fab fa-linkedin"></i>
           </a>
         </div>
       </div>
     </div>
   );
 };
-
-var textSize = {
-    fontSize: "1.5rem"
-}
 
 export default Footer;
