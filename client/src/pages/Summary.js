c83c0a69aa2b8f2efeaa5bab51c2a159a46cbf99
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-07 17:23:02 -0500

    styling complete

--- client/src/pages/Summary.js
+++ client/src/pages/Summary.js
@@ -1,63 +1,81 @@
 import React from "react";
-import { Card, Table } from "react-bootstrap";
+import { Card, Table, Button } from "react-bootstrap";
 
 const SummaryComponent = () => {
   return (
     <div>
-      <h1>Investing</h1>
-      <div className="d-flex">
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              Card Subtitle
-            </Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              Card Subtitle
-            </Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
+      <div className="w-75 mx-auto">
+        <h1 className="w-100 mx-auto" style={styles.investingTitle}>
+          Investing
+        </h1>
+        <div className="d-flex justify-content-around flex-wrap mb-5" style={styles.bordersDivs}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Company name (symbol)</Card.Title>
+              <Card.Subtitle className="mb-5 text-muted" stlye={{ height: "2rem" }}>
+                (open) Today
+              </Card.Subtitle>
+              <Button className="" href="#" block>
+                sell
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Company name (symbol)</Card.Title>
+              <Card.Subtitle className="mb-5 text-muted" stlye={{ height: "2rem" }}>
+                (open) Today
+              </Card.Subtitle>
+              <Button className="" href="#" block>
+                sell
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className=" w-100">
+          <Table className="text-center mx-auto" striped bordered hover>
+            <caption style={styles.caption}>List</caption>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="d-flex flex-column">
+                  company name
+                  <span>symbol</span>
+                </td>
+                <td>
+                  cost
+                  <span className="d-flex flex-column">difference</span>
+                </td>
+              </tr>
+              <tr>
+                <td className="d-flex flex-column">
+                  company name
+                  <span>symbol</span>
+                </td>
+                <td>
+                  cost
+                  <span className="d-flex flex-column">difference</span>
+                </td>
+              </tr>
+              <tr>
+                <td className="d-flex flex-column">
+                  company name
+                  <span>symbol</span>
+                </td>
+                <td>
+                  cost
+                  <span className="d-flex flex-column">difference</span>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       </div>
-      <Table className="w-50 text-center mx-auto" striped bordered hover>
-        <caption style={styles.caption}>List</caption>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Mark</td>
-            <td>Otto</td>
-          </tr>
-          <tr>
-            <td>Jacob</td>
-            <td>Thornton</td>
-          </tr>
-          <tr>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
     </div>
   );
 };
@@ -71,6 +89,17 @@ var styles = {
   },
   table_size: {
     fontSize: "1.5rem",
+  },
+  investingTitle: {
+    captionSide: "top",
+    fontSize: "3rem",
+    fontWeight: "bold",
+    color: "black",
+  },
+
+  bordersDivs: {
+    border: "1px solid black",
+    padding: "50px",
   },
 };
 
