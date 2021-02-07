2b0243f344ca9bccc226a8b92135bd06269dbb3b
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-07 01:34:24 -0500

    sending req to flask, getting data back

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react'
-import { Route, Switch} from 'react-router-dom'
-import Home from '../pages/Home'
-import NavbarComponent from './NavbarComponent'
-import Footer from './Footer'
-import SummaryComponent from '../pages/Summary'
-
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "../pages/Home";
+import NavbarComponent from "./NavbarComponent";
+import Footer from "./Footer";
+import SummaryComponent from "../pages/Summary";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            empty: ''
-        }
-    }
-    render() {
-        return (
-            <div style={{height: `30vh`}}>
-                <NavbarComponent />
-                <Switch>
-                    <Route path="/" exact component={() => <Home />} />
-                    <Route path="/summary" exact component={() => <SummaryComponent />} />
-                </Switch>
-                <Footer />
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      empty: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("/testing_data")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  render() {
+    return (
+      <div style={{ height: `30vh` }}>
+        <NavbarComponent />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/summary" exact component={() => <SummaryComponent />} />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default Main
+export default Main;
