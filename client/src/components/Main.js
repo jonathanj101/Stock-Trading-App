c1d2adbe6541cd1258f36b5286772ce3f1f08e60
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-12 20:10:27 -0500

    testing logic user logged

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,38 +1,43 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "../pages/Home";
-import NavbarComponent from "./NavbarComponent";
-import Footer from "./Footer";
-import SummaryComponent from "../pages/Summary";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+import NavbarComponent from './NavbarComponent';
+import Footer from './Footer';
+import SummaryComponent from '../pages/Summary';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      empty: "",
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            empty: '',
+            testing: false,
+        };
+    }
 
-  componentDidMount() {
-    fetch("/testing_data")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }
+    componentDidMount() {
+        fetch('/testing_data')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }
 
-  render() {
-    return (
-      <div style={{ height: `30vh` }}>
-        <NavbarComponent />
-        <Switch>
-          <Route path="/" exact component={() => <Home />} />
-          <Route path="/summary" exact component={() => <SummaryComponent />} />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div style={{ height: `30vh` }}>
+                <NavbarComponent isLogged={this.state.testing} />
+                <Switch>
+                    <Route path="/" exact component={() => <Home />} />
+                    <Route
+                        path="/summary"
+                        exact
+                        component={() => <SummaryComponent />}
+                    />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Main;
