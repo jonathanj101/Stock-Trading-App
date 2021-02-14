263b307748c5d640ff63e774893334f6979a2bd6
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-14 17:51:16 -0500

    form styling done

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,29 +3,91 @@ import { Form, Button } from 'react-bootstrap';
 
 const FormComponent = () => {
     return (
-        <div>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+        <div style={styles.mainDiv}>
+            <div style={styles.div}>
+                <Form style={styles.formContainer}>
+                    <div className="d-flex ">
+                        <Form.Group
+                            style={styles.formGroupStyles}
+                            className="w-50 mr-3"
+                            controlId="firstName"
+                        >
+                            <Form.Control
+                                style={styles.formControlStyles}
+                                placeholder="First name"
+                            />
+                        </Form.Group>
+                        <Form.Group
+                            style={styles.formGroupStyles}
+                            className="w-50"
+                            controlId="lastName"
+                        >
+                            <Form.Control
+                                style={styles.formControlStyles}
+                                placeholder="Last name"
+                            />
+                        </Form.Group>
+                    </div>
+                    <Form.Group controlId="email">
+                        <Form.Control
+                            style={styles.formControlStyles}
+                            type="email"
+                            placeholder="Enter email"
+                        />
+                    </Form.Group>
+                    <Form.Group
+                        style={styles.formGroupStyles}
+                        controlId="password"
+                    >
+                        <Form.Control
+                            style={styles.formControlStyles}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Group>
+                    <Button
+                        style={styles.btnStyles}
+                        variant="primary"
+                        type="submit"
+                        block
+                    >
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
+};
+
+var styles = {
+    mainDiv: {
+        width: '50%',
+        margin: '10% auto',
+    },
+    formContainer: {
+        width: '100%',
+        margin: 'auto',
+        backgroundColor: 'grey',
+    },
+    formGroupStyles: {
+        margin: '100px auto',
+    },
+    formControlStyles: {
+        fontSize: '2rem',
+    },
+    formCheckStyles: {
+        fontSize: '2rem',
+    },
+    btnStyles: {
+        width: '50%',
+        fontSize: '2rem',
+        margin: '100px auto 0 auto',
+    },
+    div: {
+        backgroundColor: 'red',
+        width: '100%',
+        height: '100%',
+    },
 };
 
 export default FormComponent;
