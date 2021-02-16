6d9f7a465889e622b68071c239206e185065d723
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-15 21:20:33 -0500

    still no updated, still testing

--- client/src/components/ProtectRoutes.js
+++ client/src/components/ProtectRoutes.js
@@ -4,7 +4,6 @@ import { Redirect } from 'react-router-dom';
 class ProtectRoute extends Component {
     render() {
         const Component = this.props.component;
-        console.log(Component);
         const isAuthenticated = false;
         return isAuthenticated ? (
             <Component />
