96592ee019e768f1cbb72504450f14dc9c41770a
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-17 16:11:59 -0500

    testing db connection

--- server/app.py
+++ server/app.py
@@ -54,4 +54,4 @@ def multiple():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
+    main.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
