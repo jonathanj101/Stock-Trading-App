56be3fc519cab8b6979af1fb1373156584f61b0b
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-18 23:24:20 -0500

    testing db connection tutorial, connected

--- server/database.py
+++ server/database.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testing.db'
-    
-    db.init_app(app)
-    
-    print('ok')
-    
-    return app
+def connect(user, password, db, host='localhost', port=5432):
+    '''Returns a connection and a metadata object'''
+    # We connect with the help of the PostgreSQL URL
+    # postgresql://federer:grandestslam@localhost:5432/tennis
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+
+    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+
+    # We then bind the connection to MetaData()
+    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+
+    return con, meta
