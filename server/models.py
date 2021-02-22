1814a7087458fcfa76d1eee2b5acacf5ab7c093c
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-21 23:02:35 -0500

    testing app connection to db

--- server/models.py
+++ server/models.py
@@ -7,3 +7,19 @@ class Testing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     rating = db.Column(db.Integer)
+    
+    
+    
+    slams = Table('slams', meta,
+        Column('name', String, primary_key=True),
+        Column('country', String)
+    )
+
+    results = Table('results', meta,
+        Column('slam', String, ForeignKey('slams.name')),
+        Column('year', Integer),
+        Column('result', String)
+    )
+
+# Create the above tables
+    meta.create_all(con)
