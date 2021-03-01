87b5f1d465bf7934b891a261a10cdc684df65e8d
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-02-28 20:17:18 -0500

    testing isAuth, missing db insertion

--- client/src/components/ProtectRoutes.js
+++ client/src/components/ProtectRoutes.js
@@ -4,7 +4,7 @@ import { Redirect } from 'react-router-dom';
 class ProtectRoute extends Component {
     render() {
         const Component = this.props.component;
-        const isAuthenticated = false;
+        const isAuthenticated = true;
         return isAuthenticated ? (
             <Component />
         ) : (
