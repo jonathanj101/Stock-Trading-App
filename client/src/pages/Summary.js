b43d8d8bb22be8067e35a664c14c867e95bb4a31
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-03-02 21:29:26 -0500

    multiple stock requests in list table

--- client/src/pages/Summary.js
+++ client/src/pages/Summary.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Card, Table, Button } from 'react-bootstrap';
 
-const SummaryComponent = ({ apple, facebook, tesla }) => {
+const SummaryComponent = ({
+    apple,
+    facebook,
+    tesla,
+    qcom,
+    american_airline,
+    microsoft,
+    sony,
+}) => {
     console.log(apple, facebook, tesla);
     return (
         <div>
             <div className="w-75 mx-auto">
                 <h1 className="w-100 mx-auto" style={styles.investingTitle}>
-                    Investing
+                    INVESTING
                 </h1>
                 <div
                     className="d-flex justify-content-around flex-wrap mb-5"
@@ -53,7 +61,7 @@ const SummaryComponent = ({ apple, facebook, tesla }) => {
                         bordered
                         hover
                     >
-                        <caption style={styles.caption}>List</caption>
+                        <caption style={styles.caption}>LIST</caption>
                         <thead>
                             <tr>
                                 <th>
@@ -67,37 +75,184 @@ const SummaryComponent = ({ apple, facebook, tesla }) => {
                         <tbody>
                             <tr>
                                 <td className="d-flex flex-column">
-                                    company name
-                                    <span>symbol</span>
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: 'bold',
+                                        }}
+                                    >
+                                        {apple.company_name}
+                                    </span>
+                                    <span>{apple.symbol}</span>
                                 </td>
                                 <td>
-                                    cost
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        ${apple.latestPrice}
+                                    </span>
+
                                     <span className="d-flex flex-column">
-                                        difference
+                                        ${apple.change}
                                     </span>
                                 </td>
                             </tr>
                             <tr>
                                 <td className="d-flex flex-column">
-                                    company name
-                                    <span>symbol</span>
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: 'bold',
+                                        }}
+                                    >
+                                        {facebook.company_name}
+                                    </span>
+                                    <span>{facebook.symbol}</span>
                                 </td>
                                 <td>
-                                    cost
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        ${facebook.latestPrice}
+                                    </span>
                                     <span className="d-flex flex-column">
-                                        difference
+                                        ${facebook.change}
                                     </span>
                                 </td>
                             </tr>
                             <tr>
                                 <td className="d-flex flex-column">
-                                    company name
-                                    <span>symbol</span>
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        {tesla.company_name}
+                                    </span>
+                                    <span>{tesla.symbol}</span>
                                 </td>
                                 <td>
-                                    cost
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        ${tesla.latestPrice}
+                                    </span>
                                     <span className="d-flex flex-column">
-                                        difference
+                                        ${tesla.change}
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="d-flex flex-column">
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        {american_airline.company_name}
+                                    </span>
+                                    <span>{american_airline.symbol}</span>
+                                </td>
+                                <td>
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        ${american_airline.latestPrice}
+                                    </span>
+                                    <span className="d-flex flex-column">
+                                        ${american_airline.change}
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="d-flex flex-column">
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        {microsoft.company_name}
+                                    </span>
+                                    <span>{microsoft.symbol}</span>
+                                </td>
+                                <td>
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        ${microsoft.latestPrice}
+                                    </span>
+                                    <span className="d-flex flex-column">
+                                        ${microsoft.change}
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="d-flex flex-column">
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        {sony.company_name}
+                                    </span>
+                                    <span>{sony.symbol}</span>
+                                </td>
+                                <td>
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        ${sony.latestPrice}
+                                    </span>
+                                    <span className="d-flex flex-column">
+                                        ${sony.change}
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="d-flex flex-column">
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        {qcom.company_name}
+                                    </span>
+                                    <span>{qcom.symbol}</span>
+                                </td>
+                                <td>
+                                    <span
+                                        style={{
+                                            fontSize: '1.50rem',
+                                            fontWeight: ' bold',
+                                        }}
+                                    >
+                                        ${qcom.latestPrice}
+                                    </span>
+                                    <span className="d-flex flex-column">
+                                        ${qcom.change}
                                     </span>
                                 </td>
                             </tr>
