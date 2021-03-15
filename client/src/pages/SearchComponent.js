47df4a4b98a013dc3a387737e8b959416cc23f3a
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-03-15 01:50:38 -0400

    change to controlled component, fetch data on user input

--- client/src/pages/SearchComponent.js
+++ client/src/pages/SearchComponent.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
-import { Form, InputGroup, Dropdown } from 'react-bootstrap';
+import { Form, InputGroup, ListGroup } from 'react-bootstrap';
 
-const SearchComponent = ({ handleChange, mainState }) => {
+const SearchComponent = ({ mainState }) => {
     const [textInput, setTextInput] = useState('');
+    const [stockSymbol, setStockSymbol] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [isStockSearched, setIsStockSearched] = useState(false);
+
     const getTextInput = (e) => {
         const { value } = e.currentTarget;
         setTextInput(value);
-        handleChange(e);
-        fetch(`/testing/${value}`)
+        sendSearchRequest(value);
+        setIsStockSearched(true);
+    };
+
+    const sendSearchRequest = (textInput) => {
+        console.log(textInput);
+        fetch(`/testing/${textInput}`)
             .then((resp) => resp.json())
-            .then((data) => console.log(data));
+            .then((data) => {
+                console.log(data);
+                setCompanyName(data.data.company_name);
+                setStockSymbol(data.data.symbol);
+            });
     };
 
     return (
@@ -17,20 +30,45 @@ const SearchComponent = ({ handleChange, mainState }) => {
             <Form.Group className=" w-75 mx-auto mt-1">
                 <InputGroup>
                     <Form.Control
+                        className=""
                         style={styles.formControl}
                         required
                         type="text"
                         name={textInput}
                         value={textInput}
                         placeholder="Search"
-                        onChange={getTextInput}
+                        onChange={(e) => {
+                            getTextInput(e);
+                        }}
                     />
                     <InputGroup.Prepend>
                         <InputGroup.Text style={styles.inputGroupText}>
-                            <i class="fas fa-search"></i>
+                            <i className="fas fa-search"></i>
                         </InputGroup.Text>
                     </InputGroup.Prepend>
                 </InputGroup>
+                {isStockSearched ? (
+                    <ListGroup flush className="d-flex flex-row">
+                        <ListGroup.Item
+                            style={styles.ListGroupItem}
+                            className=""
+                            action
+                            variant=""
+                        >
+                            {companyName}
+                        </ListGroup.Item>
+                        <ListGroup.Item
+                            style={styles.ListGroupItem2}
+                            className=""
+                            action
+                            variant=""
+                        >
+                            {stockSymbol}
+                        </ListGroup.Item>
+                    </ListGroup>
+                ) : (
+                    ''
+                )}
             </Form.Group>
         </div>
     );
@@ -45,6 +83,22 @@ const styles = {
     },
     inputGroupText: {
         fontSize: '2rem',
+    },
+    ListGroup: {
+        display: 'none',
+    },
+    ListGroupItem: {
+        borderLeft: 'none',
+        borderRight: '1px solid rgba(0,0,0,.125)',
+        borderTop: 'nonr',
+        borderBottom: 'none',
+    },
+
+    ListGroupItem2: {
+        borderLeft: '1px solid rgba(0,0,0,.125)',
+        borderRight: 'none',
+        borderTop: 'nonr',
+        borderBottom: 'none',
     },
 };
 
