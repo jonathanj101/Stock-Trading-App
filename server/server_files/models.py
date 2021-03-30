62510b5dae7805595107152a3dd86e2cd80c5074
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-03-29 23:04:56 -0400

    refactoring

--- server/server_files/models.py
+++ server/server_files/models.py
@@ -6,31 +6,28 @@ import os
 
 main = Blueprint('main', __name__)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), primary_key=True, nullable=False)
     last_name = db.Column(db.String(80), primary_key=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), primary_key=True)
-    city = db.Column(db.String(80), primary_key=True)
-    state = db.Column(db.String(80), primary_key=True)
-    zip_code = db.Column(db.Integer())
-    
-    def __init__(self,first_name, last_name, username,email, password):
+
+    def __init__(self, first_name, last_name, username, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
-        self.email = email
         self.password = password
 
     def __repr__(self):
         return '<User %r>' % self.username
-    
+
+
 class Transactions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Integer)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
+
     def __init__(self, amount):
         self.amount = amount
