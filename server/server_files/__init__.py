079c38db8d04bdf194c2915614f180a73b03e5f6
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-03-30 23:04:00 -0400

    changed database name and path

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1209lmc@localhost/testing_tutorial'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1209lmc@localhost/fantasy_stock_app'
 db = SQLAlchemy(app)
-
 from server_files import routes
