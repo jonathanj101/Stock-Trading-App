fe18b61feaa82108d21b7cc17b3a0cf24c420a96
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-03-31 19:35:31 -0400

    1-1 relation table created

--- server/server_files/models.py
+++ server/server_files/models.py
@@ -7,32 +7,25 @@ import os
 main = Blueprint('main', __name__)
 
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), primary_key=True, nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    stocks = db.relationship('Stock_purchased', backref='username', lazy=True)
-
-    def __init__(self, first_name, last_name, username, email, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.password = password
+    stocks_id = db.Column(db.Integer, db.ForeignKey("stock.id"))
+    stocks = db.relationship("Stock")
 
     def __repr__(self):
-        return f"User ({self.first_name},{self.last_name},{self.email})"
+        return f"User ('{self.first_name}','{self.last_name}','{self.email}', '{self.username}')"
 
 
-class Stock_purchased(db.Model):
+class Stock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     stock = db.Column(db.String(255), nullable=False)
-    shares = db.Column(db.String(255), nullable=False, primary_key=True)
-    user_id = db.Column(db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref='stock', lazy=True)
+    shares = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f"Stock ('{self.stock}', '{self.shares}', '{self.date}')"
@@ -42,9 +35,6 @@ class Transactions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Integer)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __init__(self, amount):
-        self.amount = amount
 
     def __repr__(self):
         return f"Transactions ('{self.amount}','{self.data}')"
