8fe5e63261862a871dc3acb38cdbff647c95ad63
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-04 20:13:51 -0400

    need relationship for users and transaction

--- server/server_files/models.py
+++ server/server_files/models.py
@@ -17,7 +17,7 @@ class Users(db.Model):
     stocks = db.relationship("Stock", backref="users")
 
     def __repr__(self):
-        return f"User ('{self.first_name}','{self.last_name}','{self.email}', '{self.username}')"
+        return f"User ('{self.first_name}','{self.last_name}','{self.email}', '{self.username}', '{self.password}')"
 
 
 class Stock(db.Model):
@@ -26,7 +26,6 @@ class Stock(db.Model):
     stock = db.Column(db.String(255), nullable=False)
     shares = db.Column(db.String(255), nullable=False)
     users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    users = db.relationship("Users", back_populates="stock")
 
     def __repr__(self):
         return f"Stock ('{self.stock}', '{self.shares}', '{self.date}')"
