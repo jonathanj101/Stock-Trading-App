78dbbd5dccde867ee317bcb81e2a4f9f6a6eb7aa
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-05 23:11:30 -0400

    User model email column change unique=false or none

--- server/server_files/models.py
+++ server/server_files/models.py
@@ -11,7 +11,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     stocks = db.relationship("Stock", backref="users")
