9f04e458cfddbfb130d4818fdce02a9453456c0b
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-07 20:59:02 -0400

    changed module name,display msg whenever server return same user error

---
+++ client/src/components/AlertMsgComponent.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Alert } from 'react-bootstrap';
+
+const AlertMsgComponent = ({ setShow, errMsg, show }) => {
+    return (
+        <div>
+            <Alert show={show} variant="danger">
+                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                <p>
+                    Change this and that and try again. Duis mollis, est non
+                    commodo luctus, nisi erat porttitor ligula, eget lacinia
+                    odio sem nec elit. Cras mattis consectetur purus sit amet
+                    fermentum.
+                </p>
+            </Alert>
+        </div>
+    );
+};
+
+export default AlertMsgComponent;
