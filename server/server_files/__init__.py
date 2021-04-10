c9dc7dd3e9fb6c040fd8deedfb751845466315ae
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-10 19:22:47 -0400

    installed flask-bcrypt, hash_password

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1209lmc@localhost/fantasy_stock_app'
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 from server_files import routes
