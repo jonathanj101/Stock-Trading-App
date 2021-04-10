99d7cc8e1f498d7dcd8c2dd2be8294fc89c72379
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-10 15:54:36 -0400

    conditional exp to show variant and msg according to msg error or success

--- client/src/components/AlertMsgComponent.js
+++ client/src/components/AlertMsgComponent.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
 
-const AlertMsgComponent = ({ errMsg, show }) => {
+const AlertMsgComponent = ({ successMsg, errMsg, show }) => {
     return (
         <div>
-            <Alert show={show} variant="danger">
-                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-                <p>{errMsg}</p>
+            <Alert
+                show={show}
+                variant={successMsg == '' ? 'danger' : 'success'}
+            >
+                <Alert.Heading>
+                    {successMsg == '' ? errMsg : successMsg}
+                </Alert.Heading>
             </Alert>
         </div>
     );
