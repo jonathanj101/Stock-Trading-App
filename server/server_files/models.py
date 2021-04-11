57a2104f62ff732b3c9e1941f41b91bea7c28f79
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-10 20:13:48 -0400

    imported login manager, implementing code

--- server/server_files/models.py
+++ server/server_files/models.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from server_files import db
+from server_files import db, login_manager
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
@@ -7,7 +8,12 @@ import os
 main = Blueprint('main', __name__)
 
 
-class Users(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
