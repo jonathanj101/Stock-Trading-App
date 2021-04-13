3da5013d5cae2bf5820baec29a661736ed5eae59
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-12 22:42:18 -0400

    tessting ProtectRoutes props

--- client/src/components/ProtectRoutes.js
+++ client/src/components/ProtectRoutes.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
-class ProtectRoute extends Component {
-    render() {
-        const Component = this.props.component;
-        console.log(this.props);
-        const isAuthenticated = false;
-        return isAuthenticated ? (
-            <Component />
-        ) : (
-            <Redirect to={{ pathname: '/page-not-found' }} />
-        );
-    }
-}
+const ProtectRoute = ({ component: Component, ...rest }) => {
+    console.log(Component);
+    console.log(rest.isLogged);
+    const isAuthenticated = false;
+
+    return isAuthenticated ? (
+        <Component />
+    ) : (
+        <Redirect to={{ pathname: '/page-not-found' }} />
+    );
+};
+
+// class ProtectRoute extends Component {
+//     render() {
+//         const Component = this.props.component;
+//         console.log(this.props);
+//         const isAuthenticated = false;
+//         return isAuthenticated ? (
+//             <Component />
+//         ) : (
+//             <Redirect to={{ pathname: '/page-not-found' }} />
+//         );
+//     }
+// }
 
 export default ProtectRoute;
