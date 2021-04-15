d7f9e91462b214af805a29d30d85096487619fef
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-14 22:31:36 -0400

    need to re-do logic for redenring component

--- client/src/components/ProtectRoutes.js
+++ client/src/components/ProtectRoutes.js
@@ -3,18 +3,8 @@ import { Redirect, Route } from 'react-router-dom';
 
 const ProtectRoute = ({ component: Component, ...rest }) => {
     debugger;
-    const isAuthenticated = false;
-    return (
-        <Route
-            render={() =>
-                isAuthenticated ? (
-                    <Component />
-                ) : (
-                    <Redirect to={{ pathname: '/page-not-found' }} />
-                )
-            }
-        />
-    );
+    // const isAuthenticated = false;
+    return <Route render={() => <Component {...rest} />} />;
     //     <Component />
     // ) : (
     //     <Redirect to={{ pathname: '/page-not-found' }} />
