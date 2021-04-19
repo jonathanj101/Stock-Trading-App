83f0d40b0fa53cac621968f7c3ebb8de2bb421f8
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-19 15:54:28 -0400

    cleaning up code

--- client/src/components/LogOutModal.js
+++ client/src/components/LogOutModal.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 
 const LogOutModal = ({ show, handleClose }) => {
     return (
         <div>
             <Modal show={show} onHide={() => handleClose(false)}>
                 <Modal.Header>
-                    <Modal.Title>successfully Loggged-off</Modal.Title>
+                    <Modal.Title>Successfully Loggged-off</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     You will be redirect to the Home page shortly!
