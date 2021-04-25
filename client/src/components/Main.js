fa1456eaac9f33fd08fcff9181cdd494fe956a94
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-25 17:46:24 -0400

    cleaning up code

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../pages/Home';
 import NavbarComponent from './NavbarComponent';
 import Footer from './Footer';
