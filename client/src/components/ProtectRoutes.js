b3eebc1e4aef4295a2e6476c69894010824999ae
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-04-27 21:54:24 -0400

    cleaning up code

--- client/src/components/ProtectRoutes.js
+++ client/src/components/ProtectRoutes.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 const ProtectRoute = ({ component: Component, isUserAuthenticated }) => {
-    debugger;
-    // console.log(Component);
-    console.log(isUserAuthenticated);
     return isUserAuthenticated ? (
         <Component />
     ) : (
