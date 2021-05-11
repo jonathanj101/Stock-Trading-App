262666b2eaef99b032cb3d1c4867847000e65ee4
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-10 22:05:49 -0400

    cleaning code

--- client/src/components/BuyStockModal.js
+++ client/src/components/BuyStockModal.js
@@ -28,7 +28,7 @@ const BuyStockModal = ({
         const localStorageUserId = JSON.parse(localStorage.getItem('userId'));
         const parsed = parseFloat(stockPrice.slice(1));
         try {
-            const request = await axios.post('/add_stock', {
+            await axios.post('/add_stock', {
                 id: localStorageUserId,
                 company_name: stockName,
                 stockCost: parsed,
