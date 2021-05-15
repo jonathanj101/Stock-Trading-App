e688b2d17463288c473225faef258b73e28c0c17
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-14 23:31:29 -0400

    update userHoldings on click

--- client/src/components/AccountDropDown.js
+++ client/src/components/AccountDropDown.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavDropdown, Button } from 'react-bootstrap';
+import axios from 'axios';
 
-const AccountDropDown = ({ username, handleLogOutOnNav, userHoldings }) => {
-    debugger;
+const AccountDropDown = ({ username, handleLogOutOnNav }) => {
+    const [userHoldings, setUserHoldings] = useState();
+
+    const updateUserHoldings = () => {
+        // debugger;
+        const localStorageUserId = JSON.parse(localStorage.getItem('userId'));
+        const fetchUserHoldings = async () => {
+            const response = await axios.post('/user', {
+                id: localStorageUserId,
+            });
+            return response;
+        };
+        fetchUserHoldings().then((data) => {
+            setUserHoldings(data.data.user_holdings);
+        });
+    };
     return (
-        <div>
-            <NavDropdown title={username} id="basic-nav-dropdown" drop="down">
+        <div id="account-navbar-dropdown">
+            <NavDropdown
+                title={username}
+                id="basic-nav-dropdown"
+                drop="down"
+                onClick={() => {
+                    console.log('clicked dropdown');
+                    updateUserHoldings();
+                }}
+            >
                 <NavDropdown.Item
-                    id="dropdown-item"
+                    // id="dropdown-item"
                     className="d-flex justify-content-center mt-5 mb-5"
                 >
                     {/* <div className="d-flex flex-wrap flex-column">
