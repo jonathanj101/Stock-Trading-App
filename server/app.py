56e2f80582ba6902d0b71c4941383f4eeb77f317
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-16 00:40:05 -0400

    imported tests dir

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 import os
 from server_files import app
+from tests.test_transactions import TestTransactions
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
