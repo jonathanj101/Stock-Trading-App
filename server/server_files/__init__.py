b12515cb3f1cb0de49f72d56bf0623f289d065e0
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-16 20:27:26 -0400

    imported pytest and impletemented code

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -1,9 +1,17 @@
+import pytest
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1209lmc@localhost/fantasy_stock_app'
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
