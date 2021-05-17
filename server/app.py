a7fd93a627e36de3cda01b270867e45b95898a94
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-16 20:25:51 -0400

    unit testing still not working

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
 import os
+# import pytest
 from server_files import app
-from tests.test_transactions import TestTransactions
+# from test_transactions import TestTransactions
+print(app)
+
+
+# @pytest.fixture
+# def client():
+#     client = app.test_client()
+
 
 if __name__ == '__main__':
+
     app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
