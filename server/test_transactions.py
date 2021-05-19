37380910bde1e5070c06193b1f3e545ad6528cc7
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-18 23:47:19 -0400

    testing

--- server/test_transactions.py
+++ server/test_transactions.py
@@ -1,40 +1,19 @@
 try:
     import pytest
     import requests
-    from app import client
+    from server_files import client
+    # from server_files.routes import configure_app
     # from server_files.routes import add_stoc
-    # print("unittest line 5 {}".format(app))
+    print("unittest line 5 {}".format(client))
 except Exception as e:
     print("Some Modules are missings {}".format(e))
 
 
-# # class TestTransactions(unittest.TestCase):
-# @pytest.fixture
 def test_transaction(client):
-    print(client)
-    landing = requests.get('http://localhost:5000/multiple_stocks')
-    # landing = client.get('/multiple_stocks')
-    # html = landing.data.decode()
-    # print(landing)
-    assert landing.status_code == 200
+    landing = client.get('http://localhost:5000/multiple_stocks')
+    response = landing.data
 
-    # print("line 18 unit test {}".format(html))
-#     client = app.test_client()
-#         # tester = app.test_client(self)
-#         # # print(tester.get("/multiple_stocks"))
-#         # # tester = 'ok'
-#         # # response = tester.get('/multiple_stocks')
-#         # # print
-#         # # status_code = response.status_code
-
-#         # print("tester unit test line 18 {}".format(tester))
-#         # # print("tester unit test line 19 {}".format(status_code))
-#         # # # self.assertEqual(status_code, 400)
-#         # # print("status code unit test line 15 {}".format(status_code))
-#         # # self.exmp_1 = add_stock(5, 5)
-#         # # result = add_stock(5, 5)
-#         # # print("unit testing line 6 {}".format(add_stock))
-#         # # print("unit testing line 7 {}".format(result))
+    assert response == 200
 
 
 if __name__ == '__main__':
