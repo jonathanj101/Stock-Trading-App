3a1e760ba71ddecce1dbdf2e4180daa129071a4a
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-19 22:55:31 -0400

    move instancetiated pytest app client

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -7,16 +7,17 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 
+@pytest.fixture
+def client():
+    app.testing = True
+    # app.config['TESTING'] = True
+    # print(app.config)
+    client = app.test_client()
+    return client
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1209lmc@localhost/fantasy_stock_app'
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 db = SQLAlchemy(app)
 from server_files import routes
-
-
-@pytest.fixture
-def client():
-    # app.testing = True
-    app.config['TESTING'] = True
-    client = app.test_client()
-    return client
