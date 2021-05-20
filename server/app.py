fc3e3dd37f1f144db66280b9f5c73e2b7dc5192b
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-19 22:54:18 -0400

    testing unit test

--- server/app.py
+++ server/app.py
@@ -1,18 +1,17 @@
 import os
 import pytest
 from server_files import app
-# from test_transactions import TestTransactions
-# print(app)
 
 
 # @pytest.fixture
 # def client():
+#     app.testing = True
 #     # app.config['TESTING'] = True
 #     client = app.test_client()
-#     print(client)
-#     return client
+#     return cli
 
 
 if __name__ == '__main__':
+    # print(app.config)
 
     app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
