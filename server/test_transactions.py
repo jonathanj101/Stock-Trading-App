665d5475bc256cf84a52575be975738a3b47cd90
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-21 12:25:46 -0400

    unit-testing with requests lib

--- server/test_transactions.py
+++ server/test_transactions.py
@@ -1,35 +1,21 @@
 try:
     import unittest
-    from server_files import app, bp
-    # from server_files import bp
-    from server_files.routes import multiple
-
-    import pytest
     import requests
-    # from unittest import mock
 except Exception as e:
     print("Some Modules are missings {}".format(e))
 
 # resp = []
 
 
-class TestSample(unittest.TestCase):
+class TestAPI(unittest.TestCase):
+    base_url = 'http://localhost:5000/'
 
-    def test_transaction(self):
-        # landing = client.get('http://localhost:5000/multiple_stocks/')
-        # client.patch.object(server_files.routes, 'multiple', 200)
-        # testing = bp.test_client()
-        # self.testing = app.create_app(config='test')
-        client = app.test_client()
-        # print(self.testing)
-        # print(bp.get('/multiple_stocks'))
-        print(client.add_url_rule('/multiple_stocks').data)
-        # resp = testing.get('http://localhost:5000/multiple_stocks/')
-        # print(resp)
-        # print(resp.status_code)
-        # assert resp.status_code == 404
-
-# def test_connection()
+    def test_multiple_stocks(self):
+        print(TestAPI.base_url)
+        response = requests.get("{}/multiple_stocks".format(TestAPI.base_url))
+        print(response)
+        # print(response.status_code)
+        "ok"
 
 
 if __name__ == '__main__':
