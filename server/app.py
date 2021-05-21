7f9997dc4d282df639476744c755a87289b99608
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-21 12:25:24 -0400

    unit-testing with requests lib

--- server/app.py
+++ server/app.py
@@ -2,16 +2,5 @@ import os
 import pytest
 from server_files import app
 
-
-# @pytest.fixture
-# def client():
-#     app.testing = True
-#     # app.config['TESTING'] = True
-#     client = app.test_client()
-#     return cli
-
-
 if __name__ == '__main__':
-    # print(app.config)
-
     app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
