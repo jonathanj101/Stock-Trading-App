b123248d878c1b51290bf36af5e665f456d8556e
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-21 12:26:04 -0400

    unit-testing with requests lib

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -1,23 +1,10 @@
-import pytest
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-bp = Blueprint('myapp', __name__)
 
 app = Flask(__name__)
-app.register_blueprint(bp)
-
-
-# @pytest.fixture
-# def client():
-#     app.testing = True
-#     # app.config['TESTING'] = True
-#     # print(app.config)
-#     client = app.test_client()
-#     return client
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1209lmc@localhost/fantasy_stock_app'
 bcrypt = Bcrypt(app)
