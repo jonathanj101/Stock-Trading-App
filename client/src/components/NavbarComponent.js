7f6ff8abf0c0f35a46af73fbffc42f8a10a0f2b2
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-26 18:22:39 -0400

    added responsive navbar

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Col, Row } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import LogInModal from './LogInModal';
 import LogOutModal from './LogOutModal';
@@ -38,7 +38,13 @@ const NavbarComponent = ({
     };
 
     return (
-        <div style={{ fontSize: '1.5rem' }}>
+        <div
+            // className="col-12 p-0"
+            style={{
+                fontSize: '1.5rem',
+                width: '100%',
+            }}
+        >
             <LogInModal
                 handleClose={handleClose}
                 show={showLogInModal}
@@ -47,58 +53,74 @@ const NavbarComponent = ({
 
             <LogOutModal show={showLogOutModal} handleClose={handleClose} />
             <Navbar
-                className="d-flex justify-content-between"
+                className="row"
+                collapseOnSelect
+                expand="sm"
+                className="d-flex justify-content-end"
                 bg="dark"
                 variant="dark"
             >
                 <Navbar.Brand></Navbar.Brand>
-                <Nav variant="pills" className="">
-                    <NavLink to="/" exact className="nav-link mr-3 ">
-                        <div>
-                            <i className="fas fa-house-user"></i>
-                            Home
-                        </div>
-                    </NavLink>
-                    <NavLink
-                        to={isUserAuthenticated ? '/my-stocks' : '/'}
-                        exact
-                        className={isUserAuthenticated ? 'nav-link mr-3' : ''}
-                    >
-                        {isUserAuthenticated ? (
-                            <div>
-                                <i className="fas fa-business-time"></i>
-                                My Stocks
-                            </div>
-                        ) : (
-                            ''
-                        )}
-                    </NavLink>
-                    {!isUserAuthenticated ? (
-                        <Nav.Item
-                            className="nav-link"
-                            onClick={() => {
-                                handleShow();
-                            }}
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse
+                    // className="col col-md-12 col-lg-12"
+                    className="justify-content-end"
+                    id="responsive-navbar-nav"
+                >
+                    <Nav className="row" variant="pills">
+                        <NavLink
+                            className="collapse navbar-collapse"
+                            to="/"
+                            exact
+                            className="nav-link mr-3 "
                         >
                             <div>
-                                <i className="fas fa-sign-in-alt"></i>
-                                Log In
+                                <i className="fas fa-house-user"></i>
+                                Home
                             </div>
-                        </Nav.Item>
-                    ) : (
-                        <Nav.Item className="d-flex align-items-center">
-                            {/* <div className="d-flex align-items-center">
-                                <i className="fas fa-user"></i>
-                            </div> */}
-                            <AccountDropDown
-                                username={username}
-                                handleLogOutOnMain={handleLogOutOnMain}
-                                setIsUserAuthenticated={setIsUserAuthenticated}
-                                setShowLogOutModal={setShowLogOutModal}
-                            />
-                        </Nav.Item>
-                    )}
-                </Nav>
+                        </NavLink>
+                        <NavLink
+                            to={isUserAuthenticated ? '/my-stocks' : '/'}
+                            exact
+                            className={
+                                isUserAuthenticated ? 'nav-link mr-3' : ''
+                            }
+                        >
+                            {isUserAuthenticated ? (
+                                <div>
+                                    <i className="fas fa-business-time"></i>
+                                    My Stocks
+                                </div>
+                            ) : (
+                                ''
+                            )}
+                        </NavLink>
+                        {!isUserAuthenticated ? (
+                            <Nav.Item
+                                className="nav-link"
+                                onClick={() => {
+                                    handleShow();
+                                }}
+                            >
+                                <div>
+                                    <i className="fas fa-sign-in-alt"></i>
+                                    Log In
+                                </div>
+                            </Nav.Item>
+                        ) : (
+                            <Nav.Item className="d-flex align-items-center">
+                                <AccountDropDown
+                                    username={username}
+                                    handleLogOutOnMain={handleLogOutOnMain}
+                                    setIsUserAuthenticated={
+                                        setIsUserAuthenticated
+                                    }
+                                    setShowLogOutModal={setShowLogOutModal}
+                                />
+                            </Nav.Item>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </div>
     );
