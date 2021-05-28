8c4e22393328e366b9dbb6f5ee3e999008926108
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-27 23:53:52 -0400

    style for having carusel and log/register form together

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,18 +3,20 @@ import { Jumbotron, Container, Carousel, Image, Card } from 'react-bootstrap';
 import tabletStocksData from '../assets/images/tablet-display-stocks.jpg';
 import bitcoinBenjamin from '../assets/images/bitcoin-benjamin.jpg';
 import laptopDisplayStock from '../assets/images/laptop-display-stocks.jpg';
+import LogInModal from '../components/LogInModal';
+import LogInComponent from '../components/LogInComponent';
 
 const Home = () => {
     return (
-        <div className="bg-primary">
-            <Jumbotron style={jumbotronStyle}>
-                <Card className="text-center" body>
-                    <h3>Fantasy Trading Stock App</h3>
-                    <h4>Personalized To Your Needs</h4>
-                    <p>To start trading, go ahead and register!</p>
-                </Card>
-                <Container>
-                    <Carousel>
+        <div style={styles.homeDiv} className="w-100 h-100 m-0 p-0 ">
+            <div className="text-center w-100">
+                <h3>Fantasy Trading Stock App</h3>
+                <h4>Personalized To Your Needs</h4>
+                <p>To start trading, go ahead and register!</p>
+            </div>
+            <div className="w-100 m-0 p-0 d-flex align-items-center">
+                <Container className="w-100 m-0 p-0">
+                    <Carousel className="w-100">
                         <Carousel.Item>
                             <Image
                                 className="d-block w-100"
@@ -35,15 +37,22 @@ const Home = () => {
                         </Carousel.Item>
                     </Carousel>
                 </Container>
-            </Jumbotron>
+                <LogInComponent />
+            </div>
         </div>
     );
 };
 
-var jumbotronStyle = {
-    margin: '0 auto 15% auto',
-    padding: '0',
-    minHeight: '30vh',
+var styles = {
+    jumbotronStyle: {
+        // margin: '0 auto 15% auto',
+        padding: '0',
+        margin: '0',
+        minHeight: '30vh',
+    },
+    homeDiv: {
+        // backgroundColor: 'black',
+    },
 };
 
 export default Home;
