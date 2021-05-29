82d586994738c3e18316f94562b228f89c08c775
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-28 23:56:51 -0400

    needs work, delete logIn Componet after success log in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Carousel, Image } from 'react-bootstrap';
 import tabletStocksData from '../assets/images/tablet-display-stocks.jpg';
 import bitcoinBenjamin from '../assets/images/bitcoin-benjamin.jpg';
 import laptopDisplayStock from '../assets/images/laptop-display-stocks.jpg';
 import LogInComponent from '../components/LogInComponent';
 
-const Home = ({ handleLogIn }) => {
+const Home = ({ handleLogIn, isLogged }) => {
     return (
-        <div style={styles.homeDiv} className="w-100 h-100 m-0 p-0 ">
+        <div className="w-100 h-100 mb-5">
             <div className="text-center w-100">
                 <h3>Fantasy Trading Stock App</h3>
                 <h4>Personalized To Your Needs</h4>
                 <p>To start trading, go ahead and register!</p>
             </div>
-            <div className="w-100 m-0 p-0 d-flex align-items-center">
-                <Container className="w-100 m-0 p-0">
+            <div className="d-flex align-items-center">
+                <Container className="w-100 m-0 p-0 mx-auto">
                     <Carousel className="w-100">
                         <Carousel.Item>
                             <Image
@@ -41,14 +41,6 @@ const Home = ({ handleLogIn }) => {
             </div>
         </div>
     );
-};
-
-var styles = {
-    jumbotronStyle: {
-        padding: '0',
-        margin: '0',
-        minHeight: '30vh',
-    },
 };
 
 export default Home;
