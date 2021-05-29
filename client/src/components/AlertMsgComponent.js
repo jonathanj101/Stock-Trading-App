8ce6eca485cec742e4e528d4e5a340c10d4f1475
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-05-28 23:54:29 -0400

    un-debugger on handleSumbit()

--- client/src/components/AlertMsgComponent.js
+++ client/src/components/AlertMsgComponent.js
@@ -5,10 +5,11 @@ const AlertMsgComponent = ({ successMsg, errMsg, show }) => {
     return (
         <div>
             <Alert
-                show={true}
+                id="alert_message_container"
+                show={successMsg}
                 variant={successMsg === '' ? 'danger' : 'success'}
             >
-                <Alert.Heading>
+                <Alert.Heading id="alert_message" style={{ fontSize: '1rem' }}>
                     {successMsg === '' ? errMsg : successMsg}
                 </Alert.Heading>
             </Alert>
