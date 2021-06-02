9418a9e13dd83b8420b55944b5b35bb73c6aa559
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-02 00:03:14 -0400

    refactoring code a bit, renaming variables

--- server/server_files/routes.py
+++ server/server_files/routes.py
@@ -8,9 +8,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from server_files import app, db, bcrypt
 from server_files.models import Users, Transactions, Stock
 
-api_key = os.environ.get('API_KEY')
+API_KEY = os.environ.get('API_KEY')
 
-base_url = "https://cloud.iexapis.com"
+BASE_URL = "https://cloud.iexapis.com"
 
 
 @ app.route("/multiple_stocks", methods=["GET"])
@@ -23,10 +23,10 @@ def multiple():
     sony = 'sne'
     american_airline = "aal"
 
-    search_url = "{}/stable/stock/market/batch?symbols={},{},{},{},{},{},{}&types=quote&token={}".format(
-        base_url, tesla, apple, fb, qcom, microsft, sony, american_airline, api_key)
+    SEARCH_URL = "{}/stable/stock/market/batch?symbols={},{},{},{},{},{},{}&types=quote&token={}".format(
+        BASE_URL, tesla, apple, fb, qcom, microsft, sony, american_airline, API_KEY)
 
-    req = requests.get(search_url)
+    req = requests.get(SEARCH_URL)
 
     resp = req.json()
 
@@ -85,9 +85,9 @@ def multiple():
 
 @ app.route("/search_stock/<string:stock>", methods=['GET'])
 def search_stock(stock):
-    search_url = "{}/stable/stock/{}/quote?token={}".format(
-        base_url, stock, api_key)
-    req = requests.get(search_url)
+    SEARCH_URL = "{}/stable/stock/{}/quote?token={}".format(
+        BASE_URL, stock, API_KEY)
+    req = requests.get(SEARCH_URL)
     resp = req.json()
     stock_data = {
         "company_name": resp["companyName"],
@@ -100,34 +100,34 @@ def search_stock(stock):
 
 @ app.route('/add_stock', methods=['POST'])
 def add_stock():
-    user_detail = request.get_json()
-    user = Users.query.filter_by(id=user_detail['id']).first()
-    if user:
+    USER_DETAILS = request.get_json()
+    USER = Users.query.filter_by(id=USER_DETAILS['id']).first()
+    if USER:
         filter_by_stock_symbol = Stock.query.filter_by(
-            stock_symbol=user_detail['stockSymbol']).first()
+            stock_symbol=USER_DETAILS['stockSymbol']).first()
         if filter_by_stock_symbol != None:
 
             update_user_cost = (filter_by_stock_symbol.user_estimated_cost +
-                                user_detail['estimatedCost'])
+                                USER_DETAILS['estimatedCost'])
             update_user_shares = (filter_by_stock_symbol.user_estimated_shares +
-                                  user_detail['estimatedShares'])
-            update_user_holdings = user.user_holdings - update_user_cost
+                                  USER_DETAILS['estimatedShares'])
+            update_user_holdings = USER.user_holdings - update_user_cost
             filter_by_stock_symbol.user_estimated_cost = update_user_cost
             filter_by_stock_symbol.user_estimated_shares = update_user_shares
-            user.user_holdings = update_user_holdings
-            transaction = Transactions(company_name=user_detail['company_name'], user_estimated_cost=user_detail['estimatedCost'],
-                                       user_holdings=user_detail['estimatedCost'], user_id=user_detail['id'])
+            USER.user_holdings = update_user_holdings
+            transaction = Transactions(company_name=USER_DETAILS['company_name'], user_estimated_cost=USER_DETAILS['estimatedCost'],
+                                       user_holdings=USER_DETAILS['estimatedCost'], user_id=USER_DETAILS['id'])
             db.session.add(transaction)
             db.session.commit()
             return "Success! Stock added to db", 200
         else:
-            user_holdings = user.user_holdings - user_detail['estimatedCost']
+            user_holdings = user.user_holdings - USER_DETAILS['estimatedCost']
             user.user_holdings = user_holdings
-            user_stock = Stock(company_name=user_detail['company_name'],
-                               stock_symbol=user_detail['stockSymbol'], stock_cost=user_detail['stockCost'],
-                               user_estimated_shares=user_detail['estimatedShares'], user_estimated_cost=user_detail['estimatedCost'], user_id=user_detail['id'])
-            transaction = Transactions(company_name=user_detail['company_name'], user_estimated_cost=user_detail[
-                'estimatedCost'], user_holdings=user_holdings, user_id=user_detail['id'])
+            user_stock = Stock(company_name=USER_DETAILS['company_name'],
+                               stock_symbol=USER_DETAILS['stockSymbol'], stock_cost=USER_DETAILS['stockCost'],
+                               user_estimated_shares=USER_DETAILS['estimatedShares'], user_estimated_cost=USER_DETAILS['estimatedCost'], user_id=USER_DETAILS['id'])
+            transaction = Transactions(company_name=USER_DETAILS['company_name'], user_estimated_cost=USER_DETAILS[
+                'estimatedCost'], user_holdings=user_holdings, user_id=USER_DETAILS['id'])
             db.session.add(user_stock)
             db.session.add(transaction)
             db.session.commit()
@@ -138,63 +138,63 @@ def add_stock():
 
 @ app.route('/sell_stock', methods=["POST"])
 def sell_stock():
-    user_detail = request.get_json()
-    user = Users.query.filter_by(id=user_detail['id']).first()
+    USER_DETAILS = request.get_json()
+    USER = Users.query.filter_by(id=USER_DETAILS['id']).first()
 
-    filter_by_stock = Stock.query.filter_by(
-        stock_symbol=user_detail['stockSymbol']).first()
+    STOCK = Stock.query.filter_by(
+        stock_symbol=USER_DETAILS['stockSymbol']).first()
 
-    search_stock = "{}/stable/stock/{}/quote?token={}".format(
-        base_url, user_detail['stockSymbol'], api_key)
+    SEARCH_STOCK = "{}/stable/stock/{}/quote?token={}".format(
+        base_url, USER_DETAILS['stockSymbol'], API_KEY)
 
-    req = requests.get(search_stock)
+    req = requests.get(SEARCH_STOCK)
 
     resp = req.json()
 
-    stock_bought_at = filter_by_stock.stock_cost
+    stock_bought_at = STOCK.stock_cost
 
     difference_in_cost = (resp['latestPrice'] -
-                          stock_bought_at) * filter_by_stock.user_estimated_shares
+                          stock_bought_at) * STOCK.user_estimated_shares
 
-    difference_in_shares = (filter_by_stock.user_estimated_cost -
-                            user_detail['userSellingAmount']) / filter_by_stock.stock_cost
-    user_holdings = user.user_holdings + \
-        difference_in_cost + user_detail['userSellingAmount']
+    difference_in_shares = (STOCK.user_estimated_cost -
+                            USER_DETAILS['userSellingAmount']) / STOCK.stock_cost
+    user_holdings = USER.user_holdings + \
+        difference_in_cost + USER_DETAILS['userSellingAmount']
 
-    if user:
-        filter_by_stock.user_estimated_cost = filter_by_stock.user_estimated_cost - \
-            user_detail['userSellingAmount']
-        filter_by_stock.user_estimated_shares = difference_in_shares
-        user.user_holdings = user_holdings
-        if filter_by_stock.user_estimated_cost == 0:
-            transaction = Transactions(company_name=user_detail['companyName'], user_estimated_cost=user_detail[
-                'userSellingAmount'], user_holdings=user_holdings, user_id=user_detail['id'])
+    if USER:
+        STOCK.user_estimated_cost = STOCK.user_estimated_cost - \
+            USER_DETAILS['userSellingAmount']
+        STOCK.user_estimated_shares = difference_in_shares
+        USER.user_holdings = user_holdings
+        if STOCK.user_estimated_cost == 0:
+            transaction = Transactions(company_name=USER_DETAILS['companyName'], user_estimated_cost=USER_DETAILS[
+                'userSellingAmount'], user_holdings=user_holdings, user_id=USER_DETAILS['id'])
             stock = Stock.query.filter_by(
-                stock_symbol=user_detail['stockSymbol']).delete()
+                stock_symbol=USER_DETAILS['stockSymbol']).delete()
             db.session.add(transaction)
             db.session.commit()
         else:
-            transaction = Transactions(company_name=user_detail['companyName'], user_estimated_cost=user_detail[
-                'userSellingAmount'], user_holdings=user_holdings, user_id=user_detail['id'])
+            transaction = Transactions(company_name=USER_DETAILS['companyName'], user_estimated_cost=USER_DETAILS[
+                'userSellingAmount'], user_holdings=user_holdings, user_id=USER_DETAILS['id'])
             db.session.add(transaction)
             db.session.commit()
 
-        return "Success!", 200
+        return ("Success!", 200)
     else:
-        return 'Looks like there is an error on our end!', 500
+        return ('Looks like there is an error on our end!', 500)
 
 
 @ app.route('/user_stock', methods=['POST'])
 def user_stock():
-    user_detail = request.get_json()
-    user = Users.query.filter_by(id=user_detail['id']).first()
-    stock = Stock.query.filter_by(user_id=user_detail['id']).all()
+    USER_DETAILS = request.get_json()
+    USER = Users.query.filter_by(id=USER_DETAILS['id']).first()
+    STOCK = Stock.query.filter_by(user_id=USER_DETAILS['id']).all()
     stock_list = []
 
-    if user:
-        for data in stock:
+    if USER:
+        for data in STOCK:
             search_url = "{}/stable/stock/{}/quote?token={}".format(
-                base_url, data.stock_symbol, api_key)
+                BASE_URL, data.stock_symbol, API_KEY)
             req = requests.get(search_url)
             resp = req.json()
             difference_in_cost = (
@@ -222,51 +222,50 @@ def user_stock():
 
 @ app.route('/signup', methods=["POST"])
 def signup():
-    user_details = request.get_json()
-    filter_user_model_by_username = Users.query.filter_by(
-        username=user_details['username']).first()
+    USER_DETAILS = request.get_json()
+    USERNAME = Users.query.filter_by(
+        username=USER_DETAILS['username']).first()
     hashed_password = bcrypt.generate_password_hash(
-        user_details['password']).decode('utf-8')
+        USER_DETAILS['password']).decode('utf-8')
 
-    if filter_user_model_by_username is None:
-        user = Users(first_name=user_details['first_name'], last_name=user_details['last_name'],
-                     email=user_details['email'], username=user_details['username'], password=hashed_password, user_holdings=user_details['userHoldings'])
+    if USERNAME is None:
+        user = Users(first_name=USER_DETAILS['first_name'], last_name=USER_DETAILS['last_name'],
+                     email=USER_DETAILS['email'], username=USER_DETAILS['username'], password=hashed_password, user_holdings=USER_DETAILS['userHoldings'])
         db.session.add(user)
         db.session.commit()
-        return jsonify("Success! You will be redirect to your account shortly!", 200, user.id)
+        MESSAGE = "Success! You will be redirect to your account shortly!"
+        return jsonify(MESSAGE, 200, user.id)
     else:
-        return jsonify("The username has already been used! Please choose another username!", 500)
+        MESSAGE = "The username has already been used! Please choose another username!"
+        return jsonify(MESSAGE, 500)
 
 
 @ app.route('/login', methods=["POST"])
 def login():
-    user_details = request.get_json()
+    USER_DETAILS = request.get_json()
 
-    user = Users.query.filter_by(username=user_details["username"]).first()
+    USER = Users.query.filter_by(username=USER_DETAILS["username"]).first()
+    if USER and bcrypt.check_password_hash(USER.password, USER_DETAILS['password']):
+        MESSAGE = "You are logged in successfully! You will be redirected to your account shortly!"
 
-    if user and bcrypt.check_password_hash(user.password, user_details['password']):
-        response = {
-            "user_id": user.id,
-            "success_msg": "You are logged in successfully! You will be redirected to your account shortly!",
-            "username": user.username
-        }
-        return jsonify(response, 200)
+        return jsonify(MESSAGE, 200, USER.id, USER.username)
     else:
-        return jsonify("We don't recognize that username or password. Please try again!", 500)
+        MESSAGE = "We don't recognize that username or password. Please try again!"
+        return jsonify(MESSAGE, 500)
 
 
 @ app.route('/user', methods=["POST"])
 def user():
-    user_detail = request.get_json()
+    USER_DETAILS = request.get_json()
 
-    user = Users.query.filter_by(id=user_detail['id']).first()
+    USER = Users.query.filter_by(id=USER_DETAILS['id']).first()
 
     user_obj = {
-        "username": user.username,
-        "user_holdings": user.user_holdings
+        "username": USER.username,
+        "user_holdings": USER.user_holdings
     }
+    if USER:
 
-    if user:
-        return jsonify(user_obj)
+        return jsonify(user_obj), 200
     else:
-        return "User not found!", 500
+        return ("User not found!"), 500
