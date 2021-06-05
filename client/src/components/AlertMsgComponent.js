c29ceee5d303218887282f7b08f70ed20832a9ba
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-05 17:37:28 -0400

    renaming variables for readability purposes

--- client/src/components/AlertMsgComponent.js
+++ client/src/components/AlertMsgComponent.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
 
-const AlertMsgComponent = ({ successMsg, errMsg, show }) => {
+const AlertMsgComponent = ({ successMessage, errorMessage, show }) => {
     return (
         <div>
             <Alert
                 id="alert_message_container"
                 show={show}
-                variant={successMsg === '' ? 'danger' : 'success'}
+                variant={successMessage === '' ? 'danger' : 'success'}
             >
                 <Alert.Heading id="alert_message" style={{ fontSize: '1rem' }}>
-                    {successMsg === '' ? errMsg : successMsg}
+                    {successMessage === '' ? errorMessage : successMessage}
                 </Alert.Heading>
             </Alert>
         </div>
