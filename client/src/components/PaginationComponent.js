1c6ef4b8042319db3811a71c620af79a4cc82759
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-06 17:17:25 -0400

    pagination logic done, gettings props frm Summary.js

--- client/src/components/PaginationComponent.js
+++ client/src/components/PaginationComponent.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
 
-const PaginationComponent = ({ investingListLength }) => {
-    console.log(investingListLength);
+const PaginationComponent = ({
+    investingListLength,
+    handlePageChange,
+    stocksPerPage,
+}) => {
     let array = [];
-    for (let i = 1; i <= Math.ceil(investingListLength / 5); i++) {
+    for (let i = 1; i <= Math.ceil(investingListLength / stocksPerPage); i++) {
         array.push(i);
     }
+
     return (
         <div>
             <Pagination>
                 <Pagination.First />
                 {array.map((number) => {
-                    return <Pagination.Item>{number}</Pagination.Item>;
+                    return (
+                        <Pagination.Item
+                            // href="!#"
+                            key={number}
+                            onClick={() => handlePageChange(number)}
+                        >
+                            {number}
+                        </Pagination.Item>
+                    );
                 })}
                 <Pagination.Last />
             </Pagination>
