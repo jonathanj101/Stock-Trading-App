241c70263051bed70b97eb890e04966a8de5c744
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-08 22:17:56 -0400

    cleaning up code

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
-# app = Flask(__name__)
 app = Flask(__name__, static_folder='../../client/build/', static_url_path='/')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1209lmc@localhost/fantasy_stock_app'
