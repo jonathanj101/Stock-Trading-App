02fecd3014f4648cda0dd3cf9941b2c6ada96741
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-10 00:14:09 -0400

    uncommented serving client index.html

--- server/server_files/routes.py
+++ server/server_files/routes.py
@@ -14,14 +14,14 @@ API_KEY = os.environ.get('API_KEY')
 BASE_URL = "https://cloud.iexapis.com"
 
 
-# @app.route("/", methods=["GET"])
-# def index():
-#     return app.send_static_file("index.html")
+@app.route("/", methods=["GET"])
+def index():
+    return app.send_static_file("index.html")
 
 
-# @app.errorhandler(404)
-# def not_found(e):
-#     return app.send_static_file('index.html')
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
 
 
 @app.route("/multiple_stocks", methods=["GET"])
