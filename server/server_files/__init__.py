2a08188060fec6947ee7f6a2a002f15f6189e543
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-10 01:18:02 -0400

    commentted out for heroku, gunicorn error importing them preventing to db.create_all()

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -15,4 +15,5 @@ else:
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-from server_files import routes
+from server.server_files import routes
+# from server_files import routes commentted out for heroku, gunicorn error importing them preventing to db.create_all()
