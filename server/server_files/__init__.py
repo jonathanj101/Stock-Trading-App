9e718d883d7667f835b6c6d49eb793e38321b3d4
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-12 22:22:24 -0400

    env db path and var re-name

--- server/server_files/__init__.py
+++ server/server_files/__init__.py
@@ -6,16 +6,16 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__, static_folder='../../client/build/', static_url_path='/')
 
 ENV = "!dev"
-DATABASE_URI = os.environ.get("POSTGRES_DATABASE_URI")
-HEROKU_DB_URI = os.environ.get("HEROKU_DATABASE_URI")
+DATABASE_URI = os.environ.get("DATABASE_URI")
+MY_POSTGRES_DB_URI = os.environ.get("MY_POSTGRES_DB_URI")
 
 
 if ENV == "dev":
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = MY_POSTGRES_DB_URI
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = HEROKU_DB_URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
