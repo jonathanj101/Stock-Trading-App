d480a5819c209d9425ac614ac711039643c38510
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-28 23:04:01 -0400

    refactored

---
+++ client/src/components/DropDown/AccountDropDownStyles.js
@@ -0,0 +1,12 @@
+export const styles = {
+    button: {
+        width: '50%',
+        margin: '50px auto 5px',
+        fontSize: '1.5rem',
+    },
+    dropdownMenu: {
+        width: '25rem',
+        textAlign: 'center',
+        fontSize: '1.5rem',
+    },
+};
