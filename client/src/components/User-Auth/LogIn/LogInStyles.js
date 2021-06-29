d1c3accd945eb9158e2714e770f7e6428ad88442
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-28 23:06:59 -0400

    refactored

---
+++ client/src/components/User-Auth/LogIn/LogInStyles.js
@@ -0,0 +1,15 @@
+export var styles = {
+    formStyle: {
+        padding: '100px',
+        width: '75%',
+        margin: 'auto',
+    },
+    formGroupStyles: {
+        marginBottom: '50px',
+    },
+    formTextStyles: {
+        fontSize: '1.75rem',
+        margin: '50px auto',
+        fontWeight: 'lighter',
+    },
+};
