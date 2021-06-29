1866d556ea2522debfaf37f3b4820c2be365fb34
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-28 23:03:11 -0400

    refactored

---
+++ client/src/components/Buy-Sell-Stocks/Sell-Stock/SellStockStyles.js
@@ -0,0 +1,10 @@
+export const styles = {
+    stockInfoDiv: {
+        text: 'center',
+        marginTop: '20px',
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+    },
+};
