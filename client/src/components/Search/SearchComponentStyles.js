b662a55d878eb66fa62bdd6d519be9b13af8751a
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-28 23:05:01 -0400

    refactored

---
+++ client/src/components/Search/SearchComponentStyles.js
@@ -0,0 +1,24 @@
+export const styles = {
+    formControl: {
+        fontSize: '2rem',
+    },
+    inputGroup: {
+        margin: '20px auto',
+    },
+    inputGroupText: {
+        fontSize: '2rem',
+    },
+    ListGroup: {
+        display: 'none',
+    },
+    spanContainer: {
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+    },
+    companyNameSpan: {
+        margin: 'auto 5% auto auto',
+    },
+    stockSymbolSpan: {
+        margin: 'auto 5% auto auto',
+    },
+};
