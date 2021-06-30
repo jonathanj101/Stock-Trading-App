2d56ffd278fdd221c45eaf43e8614aad9039a66d
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-30 00:15:43 -0400

    fixing/updating importing issues

--- client/src/pages/Summary.js
+++ client/src/pages/Summary.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchComponent from '../components/Search/SearchComponent';
-import BuyStockModal from '../components/Buy-Sell-Stocks/BuyStockModal';
-import SellStockModal from '../components/Buy-Sell-Stocks/SellStockModal';
+import BuyStockModal from '../components/Buy-Sell-Stocks/Buy-Stock/BuyStockModal';
+import SellStockModal from '../components/Buy-Sell-Stocks/Sell-Stock/SellStockModal';
 import StockListTableComponent from '../components/Stocks-Table/StockListTableComponent';
 import InvestingListTable from '../components/Stocks-Table/InvestingListTable';
 import PaginationComponent from '../components/Pagination/PaginationComponent';
